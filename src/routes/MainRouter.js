b6d54205a962a2fafb822fb3ec2b8c90b5975a20
abbassays <maliabbas2001@gmail.com>
2022-06-15 17:08:53 +0500

    modelling

--- src/routes/MainRouter.js
+++ src/routes/MainRouter.js
@@ -1,8 +1,27 @@
 const router = require("express").Router();
+const { addProduct,
+  getAllProducts,
+  getProduct,
+  updateProduct,
+  deleteProduct } = require("../controllers/productController");
+
+  
+router.post("/", addProduct);
+router.get("/test", getAllProducts);
+router.get("/product/:id", getProduct);
+// router.put("/:id", updateProduct);
+// router.delete("/:id", deleteProduct);
+
 
 router.get("/", (req, res) => {
   res.render("home.html");
 });
+
+router.get("/home", (req, res) => {
+  res.render("home.html");
+});
+
+
 
 router.get("/shop", (req, res) => {
   res.render("shop.html");
@@ -13,6 +32,6 @@ router.get("/shop/:id", (req, res) => {
   const id = req.params.id;
   console.log(id)
   res.render("product.html");
-})
+});
 
 module.exports = router;
