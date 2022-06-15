0d37d65016333f7cfe40e42550bd6247a8604c87
Asadullah Kashif <s.asad18k@gmail.com>
2022-06-15 22:34:22 +0500

    added contact page

--- src/routes/MainRouter.js
+++ src/routes/MainRouter.js
@@ -11,8 +11,12 @@ router.get("/shop", (req, res) => {
 router.get("/shop/:id", (req, res) => {
   // get the id from the url
   const id = req.params.id;
-  console.log(id)
+  console.log(id);
   res.render("product.html");
-})
+});
+
+router.get("/contact-01", (req, res) => {
+  return res.render("contact-01.html");
+});
 
 module.exports = router;
